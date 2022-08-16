22b86e7e0463dd51ec3c65ab9010786269346e38
Stefanescu Mihai <Mihai.Stefanescu@msg.group>
2022-08-16 16:44:06 +0300

    Added final styling

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -6,17 +6,26 @@ const StatusMessage = ({winner, current}) => {
     const noMovesLeft = current.board.every(element => element !== null);
 
     return (
-        <h2>
+        <div className="status-message">
             {
-                winner && `Winner is ${winner}`
+                winner && 
+                <>
+                    Winner is <span className={winner === 'X' ? 'text-green' : 'text-orange'}>{winner}</span>
+                </>
             }
             {
-                !winner && !noMovesLeft && `Next player is ${current.isXNext ? 'X' : 'O'}`
+                !winner && !noMovesLeft && 
+                <>
+                    Next player is <span className={current.isXNext ? 'text-green' : 'text-orange'}>{current.isXNext ? 'X' : 'O'}</span>
+                </>
             }
             {
-                !winner && noMovesLeft && 'X and O tied'
+                !winner && noMovesLeft && 
+                <>
+                    <span className="text-green">X</span> and <span className="text-orange">O</span> tied
+                </>
             }
-        </h2>
+        </div>
     )
 
 }
